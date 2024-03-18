d350c33b8e7a5fefaae65e3d05559517ac3a2c2b
Athreya Rayaprolu <athreya@Athreyas-MacBook-Pro.local>
2024-03-18 13:49:41 -0400

    using the dynamic pipeline concept to solve this

---
+++ test123.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eo pipefail
+set -x
+
+step_result=$(build.state)
+
+if [ "$step_result" = "passed" ]; then
+    cat <<YAML
+steps:
+  - command: "echo 'Step has passed'"
+notify:
+  - slack:
+      channels:
+        - "Support_test_account#training-and-testing"
+      message: "Hello I'm a custom success message"
+
+YAML
+else
+    cat <<YAML
+steps:
+  - command: "echo 'It failed'"
+notify:
+  - slack:
+      channels:
+        - "Support_test_account#training-and-testing"
+      message: "Hello I'm a custom failed message"
+
+YAML
+fi
