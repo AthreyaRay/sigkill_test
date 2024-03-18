74352a872f4bd1da282f9d8ac0835dbf241d2ac2
Athreya Rayaprolu <athreya@Athreyas-MacBook-Pro.local>
2024-03-18 14:09:50 -0400

    using the dynamic pipeline concept to solve this

--- test123.sh
+++ test123.sh
@@ -8,22 +8,23 @@ if [ "$step_result" = "passed" ]; then
     cat <<YAML
 steps:
   - command: "echo 'Step has passed'"
-notify:
-  - slack: "Support_test_account#training-and-testing"
-    #   channels:
-    #     - "Support_test_account#training-and-testing"
-    #   message: "Hello I'm a custom success message"
+    notify:
+    - slack:
+        channels:
+            - "Support_test_account#training-and-testing"
+        message: "Hello I'm a custom success message"
 
 YAML
 else
     cat <<YAML
 steps:
   - command: "echo 'It failed'"
-notify:
-  - slack: "Support_test_account#training-and-testing"
-    #   channels:
-    #     - "Support_test_account#training-and-testing"
-    #   message: "Hello I'm a custom failed message"
+    notify:
+    - slack:
+        channels:
+            - "Support_test_account#training-and-testing"
+        message: "Hello I'm a custom success message"
+
 
 YAML
 fi
